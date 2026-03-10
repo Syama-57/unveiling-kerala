0c6fa1fbe2b439bb009c8d16c4cb8de6a246a76e
username <email>
2026-03-10 12:50:11 +0530

    Fix headers in model

--- backend/destinations/models.py
+++ backend/destinations/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
+from geopy.geocoders import Nominatim
 
 class Story(models.Model):
     CATEGORY_CHOICES = [
