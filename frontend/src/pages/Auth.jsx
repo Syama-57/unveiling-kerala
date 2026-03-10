92da1e99bac658c17cdc074d98063432e661e392
username <email>
2026-03-10 12:33:20 +0530

    Add axios API service

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import api from "../api/api";
+import api from "../api/axios";
 import "./Auth.css";
 import Navbar from "../components/Navbar";
 
